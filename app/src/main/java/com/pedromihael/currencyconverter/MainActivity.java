280c6660fed255e6e3b0a9959c6ed070cc932519
Pedro Mihael <pedromvpaes@gmail.com>
2020-02-25 17:01:54 -0400

    Change edittext line color

--- app/src/main/java/com/pedromihael/currencyconverter/MainActivity.java
+++ app/src/main/java/com/pedromihael/currencyconverter/MainActivity.java
@@ -5,12 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
